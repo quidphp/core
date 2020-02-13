1c1647b7006a8580fe5d34c9231d1b5e2a55dfc6
Pierre-Philippe Emond <emondpph@gmail.com>
2020-02-13 12:45:00 +0000

    cli preload / php preload

--- src/Route/CliPreload.php
+++ src/Route/CliPreload.php
@@ -25,34 +25,45 @@ abstract class CliPreload extends Core\RouteAlias
     // config
     public static $config = [
         'path'=>['-preload'],
-        'target'=>'[storage]/preload.php',
         'service'=>Core\Service\PhpPreload::class,
-        'from'=>[
-            Base::class=>[
-                'priority'=>[
-                    '_root.php',
-                    'Root.php','Assoc.php','Listing.php','Set.php','Obj.php','Str.php','Finder.php','File.php','Request.php','Sql.php','Uri.php','Path.php']],
-            Test\Base::class=>[],
-            Main::class=>[
-                'priority'=>[
-                    '_root.php','_rootClone.php','File/_log.php','File/_storage.php','Map/_classeObj.php','Map/_obj.php','Map/_classe.php','Lang/_overload.php',
-                    'Root.php','ArrObj.php','ArrMap.php','Exception.php','Map.php','Res.php','File.php','Service.php','Widget.php','File/Binary.php','File/Text.php','File/Html.php','File/Dump.php','File/Log.php','File/Serialize.php','File/Json.php']],
-            Test\Main::class=>[],
-            Orm::class=>[
-                'priority'=>[
-                    '_tableAccess.php',
-                    'Relation.php','Exception.php','Pdo.php','Operation.php','RowOperation.php','TableOperation.php','Syntax.php']],
-            Test\Orm::class=>[],
-            Routing::class=>[],
-            Test\Routing::class=>[],
-            Core::class=>['closure'=>true],
-            Test\Core::class=>[],
-            '%key%'=>['closure'=>true]],
+        'compile'=>[
+            'root'=>[
+                'to'=>'[storage]/preload/quidphp.php',
+                'from'=>[
+                    Base::class=>[
+                        'priority'=>[
+                            '_root.php',
+                            'Root.php','Assoc.php','Listing.php','Set.php','Obj.php','Str.php','Finder.php','File.php','Request.php','Sql.php','Uri.php','Path.php']],
+                    Main::class=>[
+                        'priority'=>[
+                            '_root.php','_rootClone.php','File/_log.php','File/_storage.php','Map/_classeObj.php','Map/_obj.php','Map/_classe.php','Lang/_overload.php',
+                            'Root.php','ArrObj.php','ArrMap.php','Exception.php','Map.php','Res.php','File.php','Service.php','Widget.php','File/Binary.php','File/Text.php','File/Html.php','File/Dump.php','File/Log.php','File/Serialize.php','File/Json.php']],
+                    Orm::class=>[
+                        'priority'=>[
+                            '_tableAccess.php',
+                            'Relation.php','Exception.php','Pdo.php','Operation.php','RowOperation.php','TableOperation.php','Syntax.php']],
+                    Routing::class=>[]],
+                'option'=>[
+                    'credit'=>[Core\Boot::class,'quidCredit']]],
+            'init'=>[
+                'to'=>'[storage]/preload/quidphp-init.php',
+                'from'=>[
+                    Core::class=>['closure'=>true]],
+                'option'=>[
+                    'credit'=>[Core\Boot::class,'quidCredit'],
+                    'registerClosure'=>true,
+                    'bootPreload'=>true]],
+            'app'=>[
+                'to'=>'[storage]/preload/app.php',
+                'from'=>[
+                    '%key%'=>['closure'=>true,'initMethod'=>true]]]],
         'option'=>[
-            'credit'=>[Core\Boot::class,'quidCredit'],
-            'registerClosure'=>true,
-            'bootPreload'=>true,
-            'initMethod'=>'__init']
+            'initMethod'=>'__init',
+            'entry'=>[
+                'emptyLine'=>false,
+                'comment'=>false,
+                'closure'=>false,
+                'initMethod'=>false]]
     ];
 
 
@@ -72,20 +83,6 @@ abstract class CliPreload extends Core\RouteAlias
     }
 
 
-    // getTarget
-    // retourne le fichier target
-    final protected function getTarget():Main\File\Php
-    {
-        $return = null;
-        $attr = $this->getAttr('target');
-
-        if(!empty($attr))
-        $return = Core\File\Php::newCreate($attr);
-
-        return $return;
-    }
-
-
     // getService
     // retourne le service de concatenation
     final protected function getService(?array $option=null):Main\Service
@@ -99,18 +96,48 @@ abstract class CliPreload extends Core\RouteAlias
     }
 
 
+    // getTarget
+    // retourne un fichier target
+    final protected function getTarget(array $array):Main\File\Php
+    {
+        $return = null;
+        $target = $array['to'] ?? null;
+
+        if(is_string($target) && !empty($target))
+        $return = Core\File\Php::newCreate($target);
+
+        return $return;
+    }
+
+
     // getFrom
     // retourne les namespaces à compiler dans un tableau
-    final protected function getFrom():array
+    final protected function getFrom(array $array):array
     {
-        $return = $this->getAttr('from');
+        $return = null;
+        $from = $array['from'] ?? null;
 
-        if(is_array($return) && !empty($return))
+        if(is_array($from) && !empty($from))
         {
             $name = static::boot()->name(true);
+            $return = $from;
             $return = Base\Arrs::keysReplace(['%key%'=>$name],$return);
             $return = Base\Arrs::valuesReplace(['%key%'=>$name],$return);
         }
+
+        return $return;
+    }
+
+
+    // getOption
+    // retourne les options pour une compilation
+    final protected function getOption(array $array):array
+    {
+        $return = null;
+        $base = $this->getAttr('option');
+        $option = $array['option'] ?? null;
+        $option = Base\Arrs::replace($base,$option);
+        $return = Base\Call::digStaticMethod($option);
 
         return $return;
     }
@@ -121,25 +148,37 @@ abstract class CliPreload extends Core\RouteAlias
     final protected function preload():array
     {
         $return = [];
-        $method = 'neg';
-        $value = 'x';
+        $compile = (array) $this->getAttr('compile');
 
-        $target = $this->getTarget();
-        $from = $this->getFrom();
-        $option = (array) $this->getAttr('option');
-        $option = Base\Call::digStaticMethod($option);
+        foreach ($compile as $key => $array)
+        {
+            $target = $this->getTarget($array);
+            $from = $this->getFrom($array);
+            $option = $this->getOption($array);
+            ['method'=>$method,'value'=>$value] = $this->compileOne($target,$from,$option);
+
+            Cli::$method($value);
+            $return[] = [$method=>$value];
+        }
+
+        return $return;
+    }
+
+
+    // compileOne
+    // permet de faire une compilation
+    final protected function compileOne(Main\File $target,array $from,array $option):array
+    {
+        $return = ['method'=>'neg','value'=>'x'];
         $service = $this->getService($option);
         $preload = $service->trigger($from,$target);
 
         if(!empty($preload))
         {
             $target->overwrite($preload);
-            $method = 'pos';
-            $value = ['path'=>$target->path(),'size'=>$target->size()];
+            $return['method'] = 'pos';
+            $return['value'] = ['path'=>$target->path(),'size'=>$target->size()];
         }
-
-        Cli::$method($value);
-        $return[] = [$method=>$value];
 
         return $return;
     }
--- src/Service/PhpPreload.php
+++ src/Service/PhpPreload.php
@@ -25,9 +25,7 @@ class PhpPreload extends Main\Service
         'bootPreload'=>false, // s'il faut mettre preload dans core/boot
         'concatenator'=>[], // option pour le compileur, voir la méthode statique concatenatorOption
         'credit'=>null, // permet de mettre un texte de crédit en haut du fichier
-        'initMethod'=>null, // permet de spécifier la init méthode
-        'entryLineStart'=>11, // ligne de départ pour chaque fichier
-        'entryLineEnd'=>1 // ligne de fin pour chaque fichier
+        'initMethod'=>null // permet de spécifier la init méthode
     ];
 
 
@@ -96,14 +94,25 @@ class PhpPreload extends Main\Service
     final protected function entryOption(array $return):array
     {
         $return = Base\Arrs::replace($this->getAttr('concatenator/entry'),$return);
-        $closure = $return['closure'] ?? false;
         $initMethod = $this->getAttr('initMethod');
+        $withClosure = (!empty($return['closure']));
+        $withInit = (!empty($return['init']));
+        $init = ($withInit === true)? $initMethod:null;
+        $option = Base\Arrs::replace($this->getAttr('entry'),['closure'=>$withClosure,'init'=>$init]);
 
-        if($closure === true)
-        $return['content'] = static::namespaceAccoladeAutoloadClosure($initMethod);
+        $closure = function(string $content) use ($option) {
+            $return = [];
+            $info = Main\File\Php::infoFromString($content);
 
-        else
-        $return['content'] = static::namespaceAccoladeClosure();
+            if($info['name'] === 'Boot' && $info['type'] === 'class')
+            $option['closure'] = false;
+
+            $return = Main\File\Php::innerLinesFromString($content,$option);
+
+            return $return;
+        };
+
+        $return['content'] = $closure;
 
         return $return;
     }
@@ -154,73 +163,10 @@ class PhpPreload extends Main\Service
 
         $return['entry'] = [
             'separator'=>PHP_EOL.PHP_EOL,
-            'lineStart'=>$this->getAttr('entryLineStart'),
-            'lineEnd'=>$this->getAttr('entryLineEnd'),
             'extension'=>'php'
         ];
 
         return $return;
-    }
-
-
-    // namespaceAccoladeClosure
-    // retourne la closure, ceci transforme les namespace dans les classes php en namespace avec accolade
-    final public static function namespaceAccoladeClosure():\Closure
-    {
-        return function(string $return) {
-            $lines = Base\Str::lines($return);
-            if(!empty($lines[0]) && Base\Str::isEnd(';',$lines[0]))
-            {
-                $lines[0] = Base\Str::stripEnd(';',$lines[0]);
-                $lines[0] .= ' {';
-                $lines[] = '}';
-
-                $return = Base\Str::lineImplode($lines);
-            }
-
-            return $return;
-        };
-    }
-
-
-    // namespaceAccoladeAutoloadClosure
-    // comme namespaceAccoladeClosure mais en plus la classe est storé dans main/autoload setClosure
-    // possible d'init la classe si une initMethod est fourni en argument
-    final public static function namespaceAccoladeAutoloadClosure(?string $initMethod=null):\Closure
-    {
-        return function(string $return) use($initMethod) {
-            $namespaceAccoladeClosure = static::namespaceAccoladeClosure();
-            $lines = Base\Str::lines($return);
-            $namespace = Base\Str::stripStartEnd('namespace ',';',$lines[0]);
-
-            foreach ($lines as $key => $value)
-            {
-                if(Base\Str::isStart('//',$value))
-                {
-                    $class = ucfirst(Base\Str::stripStart('// ',$value));
-
-                    $newLine = ['\Quid\Main\Autoload::setClosure("'.$namespace.'","'.$class.'",function() {',''];
-                    $lines = Base\Arr::insert($key,$newLine,$lines);
-
-                    if(is_string($initMethod))
-                    {
-                        $initMethodStr = '::'.$initMethod.'();';
-                        $last = Base\Arr::valueLast($lines);
-                        if(is_string($last) && Base\Str::isEnd($initMethodStr,$last))
-                        $lines = Base\Arr::spliceIndex(-3,3,$lines);
-                    }
-
-                    $lines[] = '});';
-
-                    break;
-                }
-            }
-
-            $return = Base\Str::lineImplode($lines);
-            $return = $namespaceAccoladeClosure($return);
-
-            return $return;
-        };
     }
 }
 
